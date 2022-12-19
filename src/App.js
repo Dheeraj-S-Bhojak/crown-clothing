bf292696f02122ef242bb7fc4edf0ab172c1d234
Dheeraj s bhojak <dheerajsbhojakwork@gmail.com>
2022-12-19 13:52:55 +0530

    navigation bar and its link created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Directory from "./components/directory/directory.component.jsx";
+import NavigationBar from "./components/navigation/navigation.component.jsx";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route to="/" element={<NavigationBar />}>
+        <Route index element={<Directory />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/images/crown.svg";
+
+import "./navigation.styles.scss";
+
+const NavigationBar = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container">
+          <Logo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/contact">
+            CONTACT
+          </Link>
+          <Link className="nav-link" to="/sign_in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+export default NavigationBar;
