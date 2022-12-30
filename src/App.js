e1044a0bb94fb50a9d0bdf6e7ff166d8e75b5ccb
Dheeraj s bhojak <dheerajsbhojakwork@gmail.com>
2022-12-30 13:09:19 +0530

    shop component file create and shop data json file inseted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Directory from "./components/directory/directory.component.jsx";
 import NavigationBar from "./components/navigation/navigation.component.jsx";
+import Shop from "./pages/shop/shop.component.jsx";
 import SignIn from "./pages/sign-in/sign-in.component.jsx";
 import SignUp from "./pages/sign-up/sign-up.component.jsx";
 
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Directory />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
   );
