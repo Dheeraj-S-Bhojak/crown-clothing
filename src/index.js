0e6a1503c8239a1b1d5078fc1ea7fb20364b8ee6
Dheeraj s bhojak <dheerajsbhojakwork@gmail.com>
2022-12-31 15:59:45 +0530

    cart context created and cart component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.js";
+import { CartProvider } from "./contexts/cart.context.jsx";
 import { ProductProvider } from "./contexts/product.context.jsx";
 
 import { UserProvider } from "./contexts/user.context.jsx";
@@ -15,7 +16,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductProvider>
       </UserProvider>
     </BrowserRouter>
